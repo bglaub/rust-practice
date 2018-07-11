53921031db906a742a1ddcc771cdfbef9678f117
bglaub <blakeglaub@gmail.com>
2018-07-11 17:45:44 -0500

    Re-wrote problem 1 with sum_divisible_by function

--- bglaub/problem_1/src/main.rs
+++ bglaub/problem_1/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let mut sum = 0;
+    const LIMIT :usize = 999;
+    println!("{}", (sum_divisible_by(3, LIMIT) + sum_divisible_by(5, LIMIT) - sum_divisible_by(15, LIMIT)));
+}
 
-    for num in 1..1000 {
-        sum += if num % 3 == 0 || num % 5 == 0 { num } else { 0 }
-    }
-
-    println!("{}", sum);
+fn sum_divisible_by(divisor: usize, limit: usize) -> usize {
+   let p = limit / divisor;
+   (divisor * (p * (p + 1))) / 2
 }
